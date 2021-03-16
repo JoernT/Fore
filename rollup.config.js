51d4cbac4dc71511eaae4afdb1baada71f3def6c
Martin Middel <martin.middel@liones.nl>
2021-03-16 14:14:33 +0100

    Implement rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,15 +1,26 @@
-import commonjs from '@rollup/plugin-commonjs';
 import resolve from '@rollup/plugin-node-resolve';
 import babel from 'rollup-plugin-babel';
 
+const { module: MODULE_DEST_FILE, dependencies } = require('./package.json');
+
 export default {
-    input: 'index.js',
-    output: {
-        dir: 'output',
-        format: 'esm'
+  input: './index.js',
+  output: [
+    {
+      file: MODULE_DEST_FILE,
+      format: 'es',
+      sourcemap: true,
     },
-    plugins: [
-        babel(),
-        resolve()
-    ]
+  ],
+  external: Object.keys(dependencies),
+  plugins: [
+    resolve(),
+    babel({
+      babelrc: false,
+		exclude: 'node_modules/**',
+		// Tell babel to accept the `static READONLY_DEFAULT = false;` properties found in some places.
+		// TODO: reconsider whether that is a good idea.
+      plugins: [[require('@babel/plugin-proposal-class-properties'), { loose: true }]],
+    }),
+  ],
 };
